7b7d56109f80c0b0efb0a639159ac0afefaebf9c
Kartik Badkhal <123658876+kabadkhal@users.noreply.github.com>
2026-04-17 03:28:27 +0000

    conversion_datatypes

---
+++ 01_basics/03_conversiondatatypes.js
@@ -0,0 +1,21 @@
+//let score = '30asa'
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" -> 33
+//"33abc" -> Nan 
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+
+// Here if while conversion we need to store in in variable an then after storing number we can store it in another variable where we can convert
+//it as 
+let someNumber = 30
+let stringInLoggedin = String(someNumber)
+console.log(typeof stringInLoggedin);
