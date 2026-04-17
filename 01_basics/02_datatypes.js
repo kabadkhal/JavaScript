29d2ba5dc0044ef2e7f0ffb80f64c6867674a3fb
Kartik Badkhal <123658876+kabadkhal@users.noreply.github.com>
2026-04-17 02:31:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" ;// treat all JS code as newer Version
+
+// alert (3+3) // we are using nodejs, not the browser
+console.log(3
+    +3
+) // code should have readability.
+
+
+let name ="Kartik"
+let age = null
+let isLoggedIn = false
+let state;
+// number => to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standlone value
+// undefiend =>
+// symbol to find the individual or unquie
+
+
+//Object 
+
+
+console.log(typeof undefined);
+console.log(typeof null);
