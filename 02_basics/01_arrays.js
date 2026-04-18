48c5de7f189815bf380a6af9b9578433b35a281b
Kartik Badkhal <123658876+kabadkhal@users.noreply.github.com>
2026-04-18 08:13:57 +0000

    Basic_Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,62 @@
+//const myArr =[0,1,2,3,4,5]
+//we can acces array console.log(myArr[])
+//const myheros = ["Ironman","Hulk","Superman","spiderman","batman"]
+//console.log(myheros[1])
+// While writing string give inveterd comma then it will be treated as 
+//
+//const myArr = new Array (0,1,2,3,4,5,6)
+console.log(myArr[0])
+
+
+///Array Methods
+//myArr.push(7)
+//myArr.push(8)
+//console.log(myArr)
+//myArr.unshift(9)
+//console.log(myArr)
+//myArr.shift()
+//console.log(myArr)
+//myArr.shift()
+//console.log(myArr)
+
+
+//------------------------------------------------
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
