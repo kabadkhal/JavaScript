73be47c6e5cf04bac0d18a18efe1be8bc854acc2
Kartik Badkhal <123658876+kabadkhal@users.noreply.github.com>
2026-04-20 17:25:24 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,39 @@
+// // // function MyAdminUser(num1,num2){
+// // //     console.log(num1 + num2)
+// // // }
+// // // MyAdminUser(6,5)
+
+// // function MyAdminUser(num1,num2){
+// //     // return result
+// //     return num1 + num2
+// // }
+// // const result = MyAdminUser(6,5) 
+// // console.log("Result:", result)
+
+
+// function user(username == kartik){
+//     if (username===undefined){
+//         console.log("Please Enter your Name ")
+//         return
+//     }
+//     return`${username}just logged in `
+// }
+// console.log(user())
+
+//How to pass multiple value if we have multiple item in cart situation.
+function cart(...num){
+    return num
+}
+
+// console.log(cart(200,300,400,500))
+
+const user2 = {
+    username: "Karan",
+    price: 900
+}
+
+function HandleObject(anyobject){
+    console.log()
+}
+
+
